b06e0bf433eaa6f0e969f009186fa703ee8feb95
ratitu <rogergodoy@gmail.com>
2026-03-08 11:28:13 -0300

    Change geemap import to the main module

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import ee
-import geemap.foliumap as geemap # Versão otimizada para Streamlit
+import geemap
 import pandas as pd
 import matplotlib.pyplot as plt
 import json
